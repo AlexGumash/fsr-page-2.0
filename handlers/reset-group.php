d986afe050afe8d0939d0309412238e4d22b9753
AlexGumash <lesha.gumbin@yandex.ru>
2020-04-21 15:55:43 +0300

    fix

--- handlers/reset-group.php
+++ handlers/reset-group.php
@@ -6,6 +6,6 @@
   $result = mysqli_query($date, $query);
   $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-  header('Location: ../account.php');
+  header('Location: ../pages/account.php');
 
 ?>
